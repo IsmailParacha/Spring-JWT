a4f15f8ff19907937ad15c991be46aad5e84ec35
IsmailParacha <ismailjanparacha@gmail.com>
2022-12-15 21:33:09 +0500

    Role base Auth in simple Security Part#1

--- src/main/java/com/user/jwt/Entity/User.java
+++ src/main/java/com/user/jwt/Entity/User.java
@@ -1,5 +1,8 @@
 package com.user.jwt.Entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,5 +20,8 @@ public class User {
     private int id;
     private String name;
     private String password;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role", referencedColumnName = "id"))
+    private Set<Role> roles = new HashSet<>();
 
 }
